3ff4ca97e62f8843ceb577d2cdedb7e509ffc7e8
mutuotakelvin <kelvinmutuota749@gmail.com>
2023-01-21 11:38:51 +0300

    initial commit

---
+++ environments.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyBIH42Yut4S4_216HkvVQyBA6g6q-LbYXw",
+      authDomain: "authtest-9f90b.firebaseapp.com",
+      databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxx",
+      projectId: "authtest-9f90b",
+      storageBucket: "authtest-9f90b.appspot.com",
+      messagingSenderId: "966474579858",
+      appId: "1:966474579858:web:1fad55610f71a1df2ccb4b",
+      measurementId: "xxxxxxxxxxxxxxxx"
+    }
+  };
